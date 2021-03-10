e848414a000ebc466d40a91b0f6af4d7d0910d23
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-03-10 21:10:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+// import './App.css';
+
+import React from 'react';  
+import Sidebar from './components/Sidebar'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+
+       
+      
+      <div className="col-lg-3">
+        <div className="app__sidebar">
+        <Sidebar/>
+        </div>
+        
+      </div>
+      <div className="col-lg-9 app__main-content"> 
+        main
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
