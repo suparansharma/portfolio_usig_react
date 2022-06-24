adb2d4d0d562cbc12ec0cfcf7c2297a40b2e5adb
suparansharma <suparansharmap@gmail.com>
2022-06-24 23:31:18 +0600

    live and git hub url

--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,21 +1,21 @@
 import hospital_management_system from '../../assets/projects_images/hospital_management_system.png'
 import car from '../../assets/projects_images/car.jpg'
-import Property from '../../assets/projects_images/property.png'
+import Amazon from '../../assets/projects_images/amajon.png'
 import Tic_Tac_Toe from '../../assets/projects_images/tic_tac_toe.png'
 
 const data_projects = [
     {
-        name: 'Property Bazaar',
-        image: Property,
-        deployed_url: 'https://github.com/suparansharma/startnewwt',
-        github_url: 'https://github.com/suparansharma/startnewwt',
+        name: 'Amazon',
+        image: Amazon,
+        deployed_url: 'https://cranky-chandrasekhar-fa337b.netlify.app/',
+        github_url: 'https://github.com/suparansharma/online_super_shop_usingreact',
         category: ['VS']
     },
 
     {
         name: 'Tic_Tac_Toe',
         image: Tic_Tac_Toe,
-        deployed_url: 'https://github.com/suparansharma/Tic_Tac_Toe_game_using_React',
+        deployed_url: 'https://confident-darwin-fca464.netlify.app/',
         github_url: 'https://github.com/suparansharma/Tic_Tac_Toe_game_using_React',
         category: ['node.js','React','VS']
     },
